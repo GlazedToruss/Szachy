0470f9070ae26d06a456277a050db23d6060198f
Paweł <pawlekpl@gmail.com>
2023-06-17 20:29:46 +0200

    Added Members Class

--- users/models.py
+++ users/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Members(models.Model):
+    user_name= models.CharField(max_length=50)
+    pass_word= models.CharField(max_length=50)
+    e_mail=models.EmailField(max_length=50)
+    f_name=models.CharField(max_length=50)
+    l_name=models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.user_name
