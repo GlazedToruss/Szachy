3312fe8d8b73da1418c60f9f6ec2feecb0bd293a
pawlekpl <pawlekpl@gmail.com>
2023-06-21 20:00:31 +0200

    Added unique url

--- play/models.py
+++ play/models.py
@@ -3,7 +3,9 @@ from django.contrib.auth.models import User
 # Create your models here.
 
 class Game(models.Model):
-    id1= models.CharField(max_length=50)
-    moves=models.CharField(max_length=200)
-    player1=models.ForeignKey(User, on_delete=models.CASCADE, related_name='player1')
-    player2=models.ForeignKey(User, on_delete=models.CASCADE, related_name='player2')
+    i_d = models.CharField(max_length=50)
+    moves =models.CharField(max_length=1000)
+    player1 = models.ForeignKey(User, on_delete=models.CASCADE, related_name='player1')
+
+    def __str__(self):
+        return self.i_d
