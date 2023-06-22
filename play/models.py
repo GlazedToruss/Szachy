bf4aed3e1545ed7df97270bb24c927f0c62172e0
GlazedToruss <maciej.malek1337@wir.pl>
2023-06-22 12:35:28 +0200

    22.06v2

--- play/models.py
+++ play/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 
 class Game(models.Model):
     i_d = models.CharField(max_length=50, unique=True)
-    is_waiting = models.BooleanField(default=True)
+    is_waiting = models.CharField(max_length=10, default='True')
     moves =models.CharField(max_length=1000)
     player1 = models.ForeignKey(User, on_delete=models.CASCADE, related_name='player1', default='default_player_1')
     player2 = models.ForeignKey(User, on_delete=models.CASCADE, related_name='player2', default='default_player_2')
